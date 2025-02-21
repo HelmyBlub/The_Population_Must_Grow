daa9df6de6da966f0beb51e2143f1b54053c8b87
Helmy Blub <helmyblub@gmail.com>
2025-02-21 16:30:27 +0100

    trying around. Moving, rotating, debug text

--- src/main.zig
+++ src/main.zig
@@ -45,19 +45,47 @@ pub fn main() !void {
     }
 
     const texture = sdl.SDL_CreateTextureFromSurface(renderer, testImage);
-    //sdl.SDL_FreeSurface(testImage);
     if (texture == null) {
         std.debug.print("SDL_CreateTextureFromSurface Error: {s}\n", .{sdl.SDL_GetError()});
         return;
     }
+    sdl.SDL_DestroySurface(testImage);
     defer sdl.SDL_DestroyTexture(texture);
 
     var event: sdl.SDL_Event = undefined;
     var running = true;
+    var somevalue: u16 = 0;
+    var frames: u16 = 0;
     while (running) {
+        frames += 1;
+        somevalue = (somevalue + 1) % 1000;
         while (sdl.SDL_PollEvent(&event)) {
             if (event.type == sdl.SDL_EVENT_QUIT) {
                 running = false;
+            } else if (event.type == sdl.SDL_EVENT_KEY_DOWN) {
+                switch (event.key.key) {
+                    sdl.SDLK_UP => {
+                        std.debug.print("key up\n", .{});
+                    },
+                    sdl.SDLK_DOWN => {
+                        std.debug.print("key down\n", .{});
+                    },
+                    else => {
+                        std.debug.print("other key {}\n", .{event.key.key});
+                    },
+                }
+            } else if (event.type == sdl.SDL_EVENT_MOUSE_BUTTON_DOWN) {
+                switch (event.button.button) {
+                    1 => {
+                        std.debug.print("left mouse {d} {d}\n", .{ event.button.x, event.button.y });
+                    },
+                    3 => {
+                        std.debug.print("right mouse {d} {d}\n", .{ event.button.x, event.button.y });
+                    },
+                    else => {
+                        std.debug.print("mouse input {}\n", .{event.button.button});
+                    },
+                }
             }
         }
 
@@ -66,12 +94,30 @@ pub fn main() !void {
         _ = sdl.SDL_RenderClear(renderer);
 
         // Draw image
-        _ = sdl.SDL_RenderTexture(renderer, texture, null, null);
-
+        const stretchRect: [1]sdl.SDL_FRect = .{.{
+            .x = @as(f32, @floatFromInt(somevalue)) / 2.0,
+            .y = 50,
+            .w = window_w / 5,
+            .h = window_h / 5,
+        }};
+        const point: [1]sdl.SDL_FPoint = .{.{ .x = 0, .y = 0 }};
+        _ = sdl.SDL_RenderTexture(renderer, texture, null, &stretchRect);
+        _ = sdl.SDL_RenderTextureRotated(
+            renderer,
+            texture,
+            null,
+            &stretchRect,
+            @as(f64, @floatFromInt(somevalue)) / 2.0,
+            &point,
+            sdl.SDL_FLIP_NONE,
+        );
         // Present frame
+        _ = sdl.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        _ = sdl.SDL_RenderDebugText(renderer, 50, 50, "asd");
         if (!sdl.SDL_RenderPresent(renderer)) {
             std.debug.print("render present Error: {s}\n", .{sdl.SDL_GetError()});
             running = false;
         }
+        //sdl.SDL_Delay(5);
     }
 }
