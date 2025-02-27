68477431d795f5642424c98d108afe26611161ac
Helmy Blub <helmyblub@gmail.com>
2025-02-27 16:46:34 +0100

    zig + vectors with more complex data

--- src/tryOutZig/justZig.zig
+++ src/tryOutZig/justZig.zig
@@ -4,8 +4,7 @@ const expect = @import("std").testing.expect;
 // zig build-exe src/tryOutZig/justZig.zig
 // zig stuff i want to try
 //   - vectors
-//      - first test result: up to 2 time faster, bad vector size could also end up in slower code
-//      - try with setup data first and than check performance
+//      - try use vectors with data similar to chatSim data
 //   - debugging
 //   - multi thread
 //      - is debugging different multi threaded?
@@ -20,6 +19,15 @@ const ChatSimMap: type = struct {
 const Position: type = struct {
     x: f32,
     y: f32,
+    direction: f32,
+    speed: f32,
+};
+
+const Positions: type = struct {
+    x: [100_000]f32,
+    y: [100_000]f32,
+    direction: [100_000]f32,
+    speed: [100_000]f32,
 };
 
 const Citizen: type = struct {
@@ -30,96 +38,67 @@ const Citizen: type = struct {
 
 pub fn main() !void {
     std.debug.print("start justZig!\n", .{});
-    addAlot();
-    addAlotVector();
+    const dataAmount: usize = 100_000;
+    var dataArray: [dataAmount]Position = undefined;
+    for (&dataArray, 0..) |*data, i| {
+        data.* = .{ .x = 0, .y = @as(f32, @floatFromInt(i)), .direction = @as(f32, @floatFromInt(i)), .speed = @mod(@as(f32, @floatFromInt(i)), 4.0) + 1.0 };
+    }
+    var data2: Positions = .{ .x = undefined, .y = undefined, .direction = undefined, .speed = undefined };
+    for (0..dataAmount) |i| {
+        data2.x[i] = 0;
+        data2.y[i] = 0;
+        data2.direction[i] = 0.1 * @as(f32, @floatFromInt(i));
+        data2.speed[i] = @mod(@as(f32, @floatFromInt(i)), 4.0) + 1.0;
+    }
+    move(&dataArray);
+    moveWithVectors(&data2);
     //    first();
 }
 
-fn addAlotVector() void {
+const vectorLength = 8;
+fn moveWithVectors(positions: *Positions) void {
     const start = std.time.microTimestamp();
-    const vectorLength = 8;
-    var someVectorResult: @Vector(vectorLength, u64) = @splat(0);
-    const max: usize = 1_000_000_000 / vectorLength;
-    for (0..max) |i| {
-        const j: u32 = @intCast(i * vectorLength);
-        const indexes = std.simd.iota(u8, vectorLength);
-        someVectorResult += @as(@Vector(vectorLength, u32), @splat(j)) + indexes;
-    }
-    const total = @reduce(.Add, someVectorResult);
-    const end = std.time.microTimestamp();
-    std.debug.print("for time: {} microseconds\n", .{end - start});
-    std.debug.print("result: {d}\n", .{total});
-}
 
-fn addAlot() void {
-    var someValue: u64 = 0;
-    const start = std.time.microTimestamp();
-    for (0..1_000_000_000) |i| {
-        someValue += i;
-    }
-    const end = std.time.microTimestamp();
-    std.debug.print("for time: {} microseconds\n", .{end - start});
-    std.debug.print("result: {d}\n", .{someValue});
-}
-
-fn first() void {
-    var state: ChatSimState = .{
-        .citizens = undefined,
-    };
-
-    for (&state.citizens) |*citizen| {
-        citizen.* = createCitizen();
-    }
-    std.debug.print("First citizen: {}\n", .{state.citizens[0]});
-    for (0..10) |_| {
-        const start = std.time.microTimestamp();
-        for (0..600) |_| {
-            citizensMove(&state);
+    for (0..100) |_| {
+        const max: usize = positions.x.len / vectorLength;
+        for (0..max) |j| {
+            const index = j * vectorLength;
+            const directionVector: @Vector(vectorLength, f32) = positions.direction[index..][0..vectorLength].*;
+            const speedVector: @Vector(vectorLength, f32) = positions.speed[index..][0..vectorLength].*;
+            var posVectorX: @Vector(vectorLength, f32) = positions.x[index..][0..vectorLength].*;
+            var posVectorY: @Vector(vectorLength, f32) = positions.y[index..][0..vectorLength].*;
+            //            posVectorX += std.math.sin(@as(@Vector(vectorLength, f32), @splat(1))) * @as(@Vector(vectorLength, f32), @splat(2));
+            posVectorX += std.math.sin(directionVector) * speedVector;
+            posVectorY += std.math.cos(directionVector) * speedVector;
+            for (0..vectorLength) |i| {
+                positions.x[index + i] = posVectorX[i];
+                positions.y[index + i] = posVectorY[i];
+            }
         }
-        std.debug.print("First citizen: {}\n", .{state.citizens[0]});
-        const end = std.time.microTimestamp();
-        std.debug.print("for time: {} microseconds\n", .{end - start});
+        //const oi = 2;
+        //std.debug.print("vector data: {d}, {d}, {d}, {d}\n", .{ positions.x[oi], positions.y[oi], positions.direction[oi], positions.speed[oi] });
     }
+
+    const end = std.time.microTimestamp();
+    std.debug.print("vector time: {} microseconds\n", .{end - start});
 }
 
-fn createCitizen() Citizen {
-    return Citizen{
-        .position = .{ .x = 0, .y = 0 },
-        .moveTo = null,
-        .moveSpeed = 2,
-    };
+fn move(positions: []Position) void {
+    const start = std.time.microTimestamp();
+    for (0..100) |_| {
+        for (positions) |*pos| {
+            movePosition(pos);
+        }
+    }
+    const end = std.time.microTimestamp();
+    std.debug.print("time: {} microseconds\n", .{end - start});
 }
 
-fn citizensMove(state: *ChatSimState) void {
-    for (&state.citizens) |*citizen| {
-        citizenMove(citizen);
-    }
+fn movePosition(position: *Position) void {
+    position.x += std.math.sin(position.direction) * position.speed;
+    position.y += std.math.cos(position.direction) * position.speed;
 }
 
-fn citizenMove(citizen: *Citizen) void {
-    if (citizen.moveTo == null) {
-        const rand = std.crypto.random;
-        citizen.moveTo = .{ .x = rand.float(f32) * 100 - 50, .y = rand.float(f32) * 100 - 50 };
-    } else {
-        const direction: f32 = calculateDirection(citizen.position, citizen.moveTo.?);
-        citizen.position.x += std.math.sin(direction) * citizen.moveSpeed;
-        citizen.position.y += std.math.cos(direction) * citizen.moveSpeed;
-    }
-}
-
-fn calculateDirection(startPos: Position, targetPos: Position) f32 {
-    var direction: f32 = 0;
-    const yDiff = (startPos.y - targetPos.y);
-    const xDiff = (startPos.x - targetPos.x);
-
-    if (xDiff >= 0) {
-        if (xDiff == 0) return 0;
-        direction = -std.math.pi + std.math.atan(yDiff / xDiff);
-    } else if (yDiff < 0) {
-        direction = -std.math.atan(xDiff / yDiff) + std.math.pi / 2.0;
-    } else {
-        if (yDiff == 0) return 0;
-        direction = -std.math.atan(xDiff / yDiff) - std.math.pi / 2.0;
-    }
-    return direction;
+fn movePositionVector(positions: []Position, direction: f32, distance: f32) void {
+    positions[0].x += std.math.sin(direction) * distance;
 }
